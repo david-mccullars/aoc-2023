1442f9224d35b98358c0035f657fe88b0b99ada5
David McCullars <david.mccullars@shopify.com>
2023-12-14 11:58:39 -0600

    2023-01

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
 pub mod template;
 
-// Use this file to add helper functions and additional modules.
+#[macro_export]
+macro_rules! regex {
+    ($name:ident, $e:expr) => {
+        lazy_static::lazy_static! {
+            static ref $name: regex::Regex = regex::Regex::new($e).unwrap_or_else(|e| {
+                eprintln!("Failed to parse regex: {}", e);
+                std::process::exit(1);
+            });
+        }
+    };
+}
+
+pub fn name_to_digit(s: &str) -> Option<u32> {
+    match s {
+        "zero" => Some(0),
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        _ => None,
+    }
+}
